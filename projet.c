dca332e911fc764f2247ceb48a435b4b1f41a0a1
Ayoub742587 <cayouba062@gmail.com>
2025-07-20 19:25:01 +0000

    Add files via upload

---
+++ projet.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <string.h>
+#include "projet.h"
+
+int ajouterLivre(Livre t[], int n) {
+    if (n >= 100)
+        return n;
+
+    printf("Titre : ");
+    getchar();
+    gets(t[n].titre);
+
+    printf("Auteur : ");
+    gets(t[n].auteur);
+
+    printf("Annee : ");
+    scanf("%d", &t[n].annee);
+
+    printf("ISBN : ");
+    scanf("%d", &t[n].isbn);
+
+    t[n].emprunte = 0;
+
+    return n + 1;
+}
+
+void afficherLivres(Livre t[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d. %s - %s (%d) [%s]\n", i + 1,t[i].titre, t[i].auteur, t[i].annee,t[i].emprunte ? "Emprunte" : "Dispo");
+    }
+}
+
+void rechercherLivre(Livre t[], int n) {
+    char mot[50];
+    printf("Mot-cle (titre ou auteur) : ");
+    getchar();
+    gets(mot);
+
+    for (int i = 0; i < n; i++) {
+        if (strcmp(t[i].titre, mot) == 0 || strcmp(t[i].auteur, mot) == 0) {
+            printf("%s - %s (%d) [ISBN:%d]\n",t[i].titre, t[i].auteur, t[i].annee, t[i].isbn);
+        }
+    }
+}
+
+int supprimerLivre(Livre t[], int n) {
+    int code;
+    printf("ISBN a supprimer : ");
+    scanf("%d", &code);
+
+    for (int i = 0; i < n; i++) {
+        if (t[i].isbn == code) {
+            for (int j = i; j < n - 1; j++) {
+                t[j] = t[j + 1];
+            }
+            return n - 1;
+        }
+    }
+    return n;
+}
+
+int emprunterLivre(Livre t[], int n, Emprunt e[], int ne) {
+    int code;
+    printf("ISBN : ");
+    scanf("%d", &code);
+    getchar();
+
+    for (int i = 0; i < n; i++) {
+        if (t[i].isbn == code && t[i].emprunte == 0) {
+            t[i].emprunte = 1;
+
+            e[ne].isbn = code;
+            printf("Nom Eleve : ");
+            gets(e[ne].nom);
+
+            printf("Date : ");
+            gets(e[ne].date);
+
+            return ne + 1;
+        }
+    }
+
+    return ne;
+}
+
+void retournerLivre(Livre t[], int n) {
+    int code;
+    printf("ISBN retour : ");
+    scanf("%d", &code);
+
+    for (int i = 0; i < n; i++) {
+        if (t[i].isbn == code) {
+            t[i].emprunte = 0;
+        }
+    }
+}
+
+void afficherStat(Livre t[], int n, int ne) {
+    int dispo = 0;
+    for (int i = 0; i < n; i++) {
+        if (!t[i].emprunte)
+            dispo++;
+    }
+
+    printf("Total : %d | Empruntés : %d | Dispo : %d\n", n, ne, dispo);
+}
---
+++ projet.h
@@ -0,0 +1,22 @@
+
+typedef struct {
+    char titre[50];
+    char auteur[30];
+    int annee;
+    int isbn;
+    int emprunte;
+} Livre;
+
+typedef struct {
+    int isbn;
+    char nom[30];
+    char date[11];
+} Emprunt;
+
+int ajouterLivre(Livre[], int);
+void afficherLivres(Livre[], int);
+void rechercherLivre(Livre[], int);
+int supprimerLivre(Livre[], int);
+int emprunterLivre(Livre[], int, Emprunt[], int);
+void retournerLivre(Livre[], int);
+void afficherStat(Livre[], int, int);
